91906677adb5c52340f5b36832589e0c19f38bf1
kaurorah <khabeck@smu.edu>
2014-09-14 14:45:24 -0500

    Adapted simple roating line animation

--- p5_sketch/assets/javascript/sketch.js
+++ p5_sketch/assets/javascript/sketch.js
@@ -1,10 +1,23 @@
+var locX, locY;
+var lineX, lineY;
 
-function setup() {
-  createCanvas(500, 500);
+function setup(){
+	createCanvas(1000, 1000);
+
 }
 
-function draw() {
-rect(0,0, 200, 200);
+function draw(){
+	fill(255, 50);
+	rect(0, 0, width, height);
+	stroke(0);
+	translate(width/2, height/2);
+
+	for (var i = 1; i < 360; i++) {
+		rotate(radians(frameCount % i *0.05 ));
+		locX= cos(i)*150;
+		locY =  sin(i)*100;
+		strokeWeight(.5);
+		line(locX, locY,  cos(i)*50, sin(i)*100);
+	}
 }
-
-
+    
