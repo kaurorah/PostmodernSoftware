f43ae5448414e81eacb13eae39d947a11991a616
kaurorah <khabeck@smu.edu>
2014-09-24 13:24:29 -0500

    Added 404 errors and check for /form and /list

--- project04rack/albums.rb
+++ project04rack/albums.rb
@@ -1,14 +1,31 @@
 require 'rack'
 
-
 class HelloWorld
 
 
-  def call(env)
-  	file = File.open("index.html", "r")
-	contents=file.read 
-    [200, {"Content-Type" => "text/html"}, [contents]]
-  end
+ 	def call(env)
+  	request= Rack::Request.new(env)
+  	case request.path
+  	when "/form" then render_form(request)
+  	when "/list" then render_list(request)
+  	else render_404
+		end
+	end
+
+	def render_form(request)
+		response=Rack::Response.new
+		File.open("index.html", "rb") { |form| response.write(form.read)  }
+		response.finish
+	end
+
+	def render_list(request)
+		response = Rack::Response.new
+		response.finish
+	end
+
+	def render_404
+		[404, {"Content-Type" => "text/html"}, ["Nothing here!"]]
+	end
 end
 
 Rack::Handler::WEBrick.run HelloWorld.new, :Port => 8080
